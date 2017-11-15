e6adf40b4960ed1208b40d8449a7623501fefe12
Daniel Winkler <winkler.daniel1@gmail.com>
2017-11-15 15:08:01 +0100

    HW1L-almostdone

--- hw1_lc/overdifferencing.R
+++ hw1_lc/overdifferencing.R
@@ -6,14 +6,18 @@ set.seed(123)
 t <- 1000
 ts <- matrix(NA, t, 1)
 ts[1, ] <- rnorm(1)
+
+eps <- rnorm(t)
 for(i in 2:nrow(ts)){
-  ts[i, ] <- 0.8 * ts[i-1, ] + rnorm(1)
+  ts[i, ] <- eps[i] + 0.00001 * eps[i-1]
 }
 
-ts.plot(ts)
-
+ts.plot(diff(ts))
 acf(ts)
+acf(diff(ts, differences = 1))
 pacf(ts)
+pacf(diff(ts, differences = 1))
+
 
 diffs <- 1000
 acl1store <- matrix(NA, diffs, 1)
@@ -21,6 +25,8 @@ acl1store <- matrix(NA, diffs, 1)
 for(i in 1:diffs){
 acl1store[i, ] <- acf(diff(ts, differences = i), plot = FALSE)$acf[2]
 }
+
+ts.plot(acl1store)
 
 
 test <- 1000
