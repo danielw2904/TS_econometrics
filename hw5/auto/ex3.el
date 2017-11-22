866584b1568097fabfb2668c770d8ce88f4f40fb
Daniel Winkler <winkler.daniel1@gmail.com>
2017-11-22 15:37:50 +0100

    ...

---
+++ hw5/auto/ex3.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "ex3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "leqno")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("flexisym" "cmbase")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "verbatim"
+    "flexisym"
+    "color"
+    "breqn")
+   (TeX-add-symbols
+    '("isqrt" 1)
+    '("iexpt" 2)
+    '("isubscript" 2)
+    '("ifracn" 2)
+    '("ifracd" 2)
+    '("ifrac" 2)))
+ :latex)
+
